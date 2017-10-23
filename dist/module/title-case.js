cbbc5e038370623eda16063cf98197855434cf10
Matthew Miller <matthew@mi11er.net>
2017-10-23 14:20:01 -0400

    update

--- dist/module/title-case.js
+++ dist/module/title-case.js
@@ -4,8 +4,8 @@
  */
 
 /**
-  * These are words that generally should not be capitalized in the title.
-  */
+ * These are words that generally should not be capitalized in the title.
+ */
 var smallWords = /^(a|an|and|as|at|but|by|en|for|if|in|nor|of|on|or|per|the|to|vs?\.?|via)$/i;
 
 /**
